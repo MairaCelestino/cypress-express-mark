6f27ed4537cfc8705e073095b0c8a290a9684750
Maira Celestino <mairacelestino@hotmail.com>
2023-12-12 09:36:30 +0000

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
+import configureAllureAdapterPlugins from '@mmisty/cypress-allure-adapter/plugins';
 const { defineConfig } = require("cypress");
+//const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+
 
 module.exports = defineConfig({
+  video:true,
   e2e: {
+    baseUrl: 'http://localhost:8080',
+    env: {
+      apiUrl: 'http://localhost:3333'
+    },
+   // viewportWidth: 1990,
+    //viewportHeight: 1100,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+         configureAllureAdapterPlugins(on, config);
+        // allureWriter(on, config);
+          return config;
     },
   },
 });
