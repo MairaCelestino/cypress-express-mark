caf95a8f5c1c7da151c5f31229ba43754a8ca05d
Maira Celestino <mairacelestino@hotmail.com>
2023-12-12 09:37:28 +0000

    Add files via upload

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -5,31 +5,101 @@
 
 describe('tarefas', () => {
 
-    it('deve cadastrar uma nova tarefa', () => {
+    let testData;
 
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: { name: 'Ler um livro de Java' }
-        }).then(response => {
-            expect(response.status).to.eq(204)
+    before(()=> {
+        cy.fixture('tasks').then(t => {
+            testData = t
         })
-
-
-        cy.visit('http://localhost:8080/')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            // .type(faker.music.songName())
-            .type('Ler um livro de Java')
-
-        cy.contains('button', 'Create').click()
-
-        /* cy.get('main div p')
-             .should('be.visible')
-             .should('have.text', 'Ler um livro de Java')*/
-
-        cy.contains('main div p', 'Ler um livro de Java')
-            .should('be.visible')
+         
     })
 
+    beforeEach(() => {
+       // cy.viewport(1990, 1100)
+    })
+
+
+    context('cadastro', () => {
+        it('deve cadastrar uma nova tarefa', () => {
+
+            const taskName = 'Ler um livro de Java'
+
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+
+            cy.contains('main div p', 'task')
+                .should('be.visible')
+        })
+
+        it('não deve permirtir tarefas duplicada', () => {
+
+            const task = testData.dup
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
+
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+
+        })
+
+        it('campo obrigatório', () => {
+
+            cy.createTask()
+            cy.isRequired('This is a required field')
+
+        })
+
+    })
+
+    context('atualização', () => {
+        it('deve concluir uma tarefa', () => {
+
+            const task = {
+                name: 'pagar contas de consumo',
+                is_done: false
+            }
+
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+
+            cy.visit('/')
+
+            cy.contains('p', task.name)
+                .parent()
+                .find('button[class*=ItemToggle]')
+                .click()
+
+            cy.contains('p', task.name)
+                .should('have.css', 'text-decoration-line', 'line-through')
+
+        })
+
+    context('exclusão', () => {
+        it('deve remover uma tarefa', () => {
+
+                const task = {
+                    name: 'Estudar JavaScript',
+                    is_done: false  
+                }
+
+                cy.removeTaskByName(task.name)
+                cy.postTask(task)
+
+                cy.visit('/')
+
+                cy.contains('p', task.name)
+                    .parent()
+                    .find('button[class*=ItemDelete]')
+                    .click()
+
+                cy.contains('p', task.name)
+                    .should('not.exist')
+
+            })
+        })
+
+    })
 })
